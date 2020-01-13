2ac1aa8826db1aabb7b6f4a6c8210e2b481b629b
JG <noreply@jg.com>
2020-01-12 18:08:15 -0800

    added query to show all employees and add role prompts

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+let action = "start";
+
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
@@ -17,11 +19,12 @@ var connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+  console.log("connected as id " + connection.threadId);
   // run the start function after the connection is made to prompt the user
   start();
+
 });
 
 // function which prompts the user for what action they should take
@@ -31,25 +34,36 @@ function start() {
       name: "action",
       type: "list",
       message: "Would you like to do?",
-      choices: ["View All Emloyees","View All Roles","View All Departments","Add Employee", "Add Department", "Add Role","EXIT"]
+      choices: ["View All Emloyees", "View All Roles", "View All Departments", "Add Employee", "Add Department", "Add Role", "Update an Employee Role", "EXIT"]
     })
-    .then(function(answer) {
+    .then(function (answer) {
       // based on their answer, call the function to handle the action
       console.log(answer);
-      switch(answer.action){
+      switch (answer.action) {
         case "View All Emloyees":
-          //call showAllEmployees
-          console.log("showallemp")
           showAllEmployees();
-        break;
+          // start();
+          break;
         case "View All Roles":
           showAllRoles();
-        break;
+          break;
         case "View All Departments":
           showAllDept();
           break;
-       default:
-        connection.end();
+        case "Add Department":
+          addDept();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+
+        case "Exit":
+          console.log("Exiting System....Goodbye!")
+          action = "exit";
+        // connection.end();
+        default:
+          // connection.end();
+          action = "exit";
       }
     });
 }
@@ -58,35 +72,132 @@ function start() {
 
 
 function showAllEmployees() {
-  connection.query("SELECT * FROM employee", function(err, res) {
+  // connection.query("SELECT * FROM employee", function (err, res) {
+    const queryString = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name, ' ', manager.last_name)  AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id";
+    connection.query(queryString, function (err, res) {
     if (err) throw err;
     console.log(res);
     console.table(res);
     connection.end();
   });
-
-
 }
 
-function showAllRoles(){
-  connection.query("SELECT * FROM role", function(err, res) {
+function showAllRoles() {
+  connection.query("SELECT * FROM role", function (err, res) {
     if (err) throw err;
     console.log(res);
     console.table(res);
     connection.end();
   });
-
 }
 
-function showAllDept(){
-  connection.query("SELECT * FROM department", function(err, res) {
+function showAllDept() {
+  connection.query("SELECT * FROM department", function (err, res) {
     if (err) throw err;
-    console.log(res);
+    // console.log(res);
     console.table(res);
-    connection.end();
+
+    //connection.end();
+
   });
+}
+
+function addDept() {
+  inquirer
+    .prompt({
+      name: "dept",
+      type: "input",
+      message: "What is the name of the new department to be added?"
+    })
+    .then(function (answer) {
+      let newdept = answer.dept;
+      connection.query("INSERT INTO department(name) values('" + newdept + "')", function (err, res) {
+
+        if (err) throw err;
+        // console.log(res);
+        console.log("New department" + newdept + "added");
+        // console.table(res);
+        connection.end();
+      });
+      showAllDept();
+      start();
+    });
 
 }
+
+// function addRole(){
+// //  console.log("addrole");
+// connection.query("SELECT * FROM department", function (err, results) {
+//   if (err) throw err;
+//   // once you have the roles, prompt the user for which role they are 
+//   inquirer
+//     .prompt([
+//       {
+//         name: "department",
+//         type: "rawlist",
+//         choices: function () {
+//           var choiceArray = [];
+//           for (var i = 0; i < results.length; i++) {
+//             choiceArray.push(results[i].name);
+//           }
+//           return choiceArray;
+//         }
+//       },
+//       {
+//         name: "title",
+//         type: "input",
+//         message: "Enter the title?"
+//       },
+//       {
+//         name: "salary",
+//         type: "input",
+//         message: "Enter the salary in format ex:15000.00"
+//       }
+//     ])
+//     .then(function (answer) {
+//       let number = answer.salary;
+//       let salary = parseFloat(answer.salary);
+//       console.log(salary)
+//       salary = Math.round(number * 100) / 100
+//       console.log(salary);
+//       console.log("title = " + answer.title);
+//       console.log("salary = " + salary);
+//       //  salary= salary.toFixed(2);
+//       //  connection.query("INSERT INTO role(title,salary,department_id) values('" +answer.title  + "',' +salary+'+ answer2.dept_ide)", function (err, res) {
+
+//       //    if (err) throw err;
+//       //    // console.log(res);
+//       //    console.log("New role added");
+//       //    // console.table(res);
+//       //    connection.end();
+//       //  });
+//       //  showAllRoles();
+//     });
+//   }
+  
+
+
+
+ function addEmployee() {
+
+    const query = connection.query(
+      "INSERT INTO employee SET ?",
+      {
+        first_name: "Noah",
+        last_name: "Bear",
+        role_id: "4",
+        manager_id: "1",
+      },
+      function (err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " employee inserted!\n");
+        // Call updateemployee AFTER the INSERT completes
+        showEmployees();
+        start();
+      });
+
+  }
+
 // // function to handle posting new items up for auction
 // function postAuction() {
 //   // prompt for info about the item being put up for auction
@@ -134,65 +245,67 @@ function showAllDept(){
 //     });
 // }
 
-// function bidAuction() {
-//   // query the database for all items being auctioned
-//   connection.query("SELECT * FROM auctions", function(err, results) {
-//     if (err) throw err;
-//     // once you have the items, prompt the user for which they'd like to bid on
-//     inquirer
-//       .prompt([
-//         {
-//           name: "choice",
-//           type: "rawlist",
-//           choices: function() {
-//             var choiceArray = [];
-//             for (var i = 0; i < results.length; i++) {
-//               choiceArray.push(results[i].item_name);
-//             }
-//             return choiceArray;
-//           },
-//           message: "What auction would you like to place a bid in?"
-//         },
-//         {
-//           name: "bid",
-//           type: "input",
-//           message: "How much would you like to bid?"
-//         }
-//       ])
-//       .then(function(answer) {
-//         // get the information of the chosen item
-//         var chosenItem;
-//         for (var i = 0; i < results.length; i++) {
-//           if (results[i].item_name === answer.choice) {
-//             chosenItem = results[i];
-//           }
-//         }
+ function addRole() {
+ // query the database for all department Id being auctioned
+  connection.query("SELECT * FROM department", function(err, results) {
+    if (err) throw err;
+    // once you have the roles, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].name);
+            }
+            return choiceArray;
+          },
+          message: "What department would you like to add the employes?"
+        },
+        {
+          name: "title",
+          type: "input",
+          message: "What is the title of the role?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        var dept_id;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].name === answer.choice) {
+            chosenItem = results[i];
+            dept_id = results[i].id;
+          }
+        }
 
-//         // determine if bid was high enough
-//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
-//           // bid was high enough, so update db, let the user know, and start over
-//           connection.query(
-//             "UPDATE auctions SET ? WHERE ?",
-//             [
-//               {
-//                 highest_bid: answer.bid
-//               },
-//               {
-//                 id: chosenItem.id
-//               }
-//             ],
-//             function(error) {
-//               if (error) throw err;
-//               console.log("Bid placed successfully!");
-//               start();
-//             }
-//           );
-//         }
-//         else {
-//           // bid wasn't high enough, so apologize and start over
-//           console.log("Your bid was too low. Try again...");
-//           start();
-//         }
-//       });
-//   });
-// }
+        // determine if bid was high enough
+        // if (chosenItem.highest_bid < parseInt(answer.bid)) {
+        //   // bid was high enough, so update db, let the user know, and start over
+        //   connection.query(
+        //     "UPDATE auctions SET ? WHERE ?",
+        //     [
+        //       {
+        //         highest_bid: answer.bid
+        //       },
+        //       {
+        //         id: chosenItem.id
+        //       }
+        //     ],
+        //     function(error) {
+        //       if (error) throw err;
+        //       console.log("Bid placed successfully!");
+        //       start();
+        //     }
+        //   );
+        // }
+        // else {
+        //   // bid wasn't high enough, so apologize and start over
+        //   console.log("Your bid was too low. Try again...");
+        //   start();
+        // }
+      });
+  });
+}
